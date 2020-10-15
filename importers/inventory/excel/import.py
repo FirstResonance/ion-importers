10b0e03035ae9d90ba47d303d1391f6d8109452e
Connor Francis <connorkfrancis94@gmail.com>
2020-10-14 19:09:59 -0700

    ION 1070: Bulk import part and inventory from excel

---
+++ importers/inventory/excel/import.py
@@ -0,0 +1,151 @@
+import argparse
+from getpass import getpass
+import logging
+import pandas as pd
+import sys
+import os
+sys.path.append(os.getcwd())
+from importers import Api, mutations # noqa
+
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s] - [%(levelname)s] - %(message)s')
+
+
+def _get_parts(api: Api, df: pd.DataFrame) -> dict:
+    """
+    Get ids for all part numbers in the excel sheet.
+
+    Args:
+        api (Api): API instance to send authenticated requests
+        df (pd.DataFrame): Dataframe of batch run creation CSV
+
+    Returns:
+        dict: Mapping from part number to part id
+    """
+    query_info = {
+        'query': mutations.GET_PARTS,
+        'variables': {
+            'filters': {'partNumber': {'in': df['Part Number'].unique().tolist()}}
+        }
+    }
+    query_data = api.send_api_request(query_info)
+    return {edge['node']['partNumber']: edge['node']['id']
+            for edge in query_data['data']['parts']['edges']}
+
+
+def _bulk_create_part_inventories(api: Api, df: pd.DataFrame, parts: dict) -> bool:
+    """
+    Bulk create inventory items for every row in excel.
+
+    Validates that the part number the inventory object is referencing exists.
+
+    Args:
+        api (Api): API instance to send authenticated requests
+        df (pd.DataFrame): Dataframe of batch run creation CSV
+        parts (dict): Mapping from part number to part id
+
+    Returns:
+        bool: True if inventory import was successful.
+    """
+    create_mutations = []
+    for _, row in df.iterrows():
+        if row['Part Number'] not in parts:
+            logging.warning('Cannot create inventory because part '
+                            f'{row["Part Number"]} does not exist.')
+            continue
+        quantity = row['Quantity'] if row['Serial Number'] is None else 1
+        mutation_input = {
+            'serialNumber': row['Serial Number'], 'quantity': quantity,
+            'partId': parts[row['Part Number']], 'lotNumber': row['Lot Number']}
+        create_mutations.append(
+            {'query': mutations.CREATE_PART_INVENTORY,
+             'variables': {'input': mutation_input}})
+    inventories = api.send_api_request(create_mutations)
+    for idx, inventory in enumerate(inventories):
+        if 'errors' in inventory and len(inventory['errors']) > 0:
+            logging.warning(inventory['errors'][0]['message'])
+    return True
+
+
+def _bulk_create_parts(api: Api, df: pd.DataFrame, parts: dict) -> bool:
+    """
+    Batch create parts for every row in the import excel.
+
+    Args:
+        api (Api): API instance to send authenticated requests
+        df (pd.DataFrame): Dataframe of batch run creation CSV
+        parts (dict): Mapping from part number to part id
+
+    Returns:
+        bool: True if part import was successful.
+    """
+    create_mutations = []
+    for _, row in df.iterrows():
+        if row['Part Number'] in parts:
+            logging.warning(
+                f'Cannot create part {row["Part Number"]} because it already exists.')
+        mutation_input = {
+            'partNumber': row['Part Number'], 'description': row['Description'],
+            'trackingType': row['Tracking Level'].upper()}
+        create_mutations.append(
+            {'query': mutations.CREATE_PART, 'variables': {'input': mutation_input}})
+    parts = api.send_api_request(create_mutations)
+    for idx, part in enumerate(parts):
+        if 'errors' in part and len(part['errors']) > 0:
+            logging.warning(part['errors'][0]['message'])
+    return True
+
+
+def import_parts(api: Api, input_file: str) -> bool:
+    """
+    Batch import part objects from excel.
+
+    Args:
+        api (Api): API instance to send authenticated requests
+        input_file (str): Location of CSV to be imported
+
+    Returns:
+        bool: True if import is successful
+    """
+    df = pd.read_excel(input_file, dtype={'Part Number': str})
+    df = df.where(df.notnull(), None)
+    parts_dict = _get_parts(api, df)
+    return _bulk_create_parts(api, df, parts_dict)
+
+
+def import_inventory(api: Api, input_file: str) -> bool:
+    """
+    Batch import inventory from excel.
+
+    Args:
+        api (Api): API instance to send authenticated requests
+        input_file (str): Location of CSV to be imported
+
+    Returns:
+        bool: True if import is successful
+    """
+    df = pd.read_excel(input_file, dtype={'Part Number': str, 'Serial Number': str})
+    df = df.where(df.notnull(), None)
+    parts_dict = _get_parts(api, df)
+    return _bulk_create_part_inventories(api, df, parts_dict)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Batch create parts and part inventory from CSV.')
+    parser.add_argument('input_file', type=str,
+                        help='Path to import Excel file.')
+    parser.add_argument('--client_id', type=str, help='Your API client ID')
+    parser.add_argument(
+        '--type', type=str, default='parts', choices=['parts', 'inventory'],
+        help='Type of file to import either parts or inventory.')
+    args = parser.parse_args()
+    client_secret = getpass('Client secret: ')
+    if not args.client_id or not client_secret:
+        raise argparse.ArgumentError('Must input client ID and '
+                                     'client secret to run import')
+    api = Api(client_id=args.client_id, client_secret=client_secret)
+    if args.type == 'inventory':
+        import_inventory(api, args.input_file)
+    else:
+        import_parts(api, args.input_file)
